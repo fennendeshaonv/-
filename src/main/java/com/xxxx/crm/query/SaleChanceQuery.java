4ab7ba43d726a60410747098515827c3f1006842
520 <1314>
2023-02-17 20:58:08 +0800

    Initial Commit

---
+++ src/main/java/com/xxxx/crm/query/SaleChanceQuery.java
@@ -0,0 +1,59 @@
+package com.xxxx.crm.query;
+
+
+import com.xxxx.base.BaseQuery;
+
+public class SaleChanceQuery extends BaseQuery {
+
+    private String customerName;
+
+    private String createMan;
+
+    private String state;
+
+
+    private Integer assignMan;
+
+
+    private Integer devResult;
+
+    public Integer getAssignMan() {
+        return assignMan;
+    }
+
+    public void setAssignMan(Integer assignMan) {
+        this.assignMan = assignMan;
+    }
+
+    public Integer getDevResult() {
+        return devResult;
+    }
+
+    public void setDevResult(Integer devResult) {
+        this.devResult = devResult;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCreateMan() {
+        return createMan;
+    }
+
+    public void setCreateMan(String createMan) {
+        this.createMan = createMan;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
